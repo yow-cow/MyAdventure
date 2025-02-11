fd88fc8df9dff3f014c6b734c30fc6ad6042bf9e
yow-cow <charlesyow@gmail.com>
2025-02-11 14:04:58 -0600

    Initial Commit

---
+++ MyAdventure/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MyAdventure
+//
+//  Created by Charles Yow on 2/11/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ MyAdventureTests/MyAdventureTests.swift
@@ -0,0 +1,17 @@
+//
+//  MyAdventureTests.swift
+//  MyAdventureTests
+//
+//  Created by Charles Yow on 2/11/25.
+//
+
+import Testing
+@testable import MyAdventure
+
+struct MyAdventureTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
